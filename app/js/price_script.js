a0402d886b93d2dfa0885e66552e6621ed2f3e9b
Anna_Kozhel <anna.kozhe.pm.2020@lpnu.ua>
2021-11-29 13:19:43 +0200

    Лабораторні роботи

--- app/js/price_script.js
+++ app/js/price_script.js
@@ -1,28 +1,20 @@
+var area1; 
+var sum; 
+var option; 
+function read0(inputs){ 
+    area1 = inputs.value; 
+    console.log(area1); 
+} 
+function read1(el){  
+    console.log(area1); 
+    option = parseInt(el.id); 
+    console.log(option); 
+    sum = area1*option; 
+    console.log(sum); 
+    //console.log(document.querySelector("#getDiv span[id]")); 
+    document.querySelector("#getDiv span[id]").innerHTML=("Приблизна собівартість:"+sum.toString()); 
+} 
  
-var area1;
-
-var sum;
-var option;
-function read0(inputs){
-    area1 = inputs.value;
-    console.log(area1);
+function read2(elem){  
+    console.log(elem); 
 }
-function read1(el){ 
-    console.log(area1);
-   
-    option = parseInt(el.id);
-    console.log(option);
-    sum = area1*option;
-    console.log(sum);
-    //console.log(document.querySelector("#getDiv span[id]"));
-    console.log(document.querySelector("#getDiv span[id]").innerHTML);
-    document.querySelector("#getDiv span[id]").innerHTML+=sum.toString();
-    console.log(document.querySelector("#getDiv span[id]").innerHTML);
-}
-
-function read2(elem){ 
-    console.log(elem);
-}
-
-
-
