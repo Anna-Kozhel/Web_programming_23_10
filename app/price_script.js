1588fd9a2d2edc7be97717e0aa9a49da0ddf8f08
Anna_Kozhel <anna.kozhe.pm.2020@lpnu.ua>
2021-11-29 12:20:29 +0200

    Лабораторні роботи

--- app/price_script.js
+++ app/price_script.js
@@ -9,15 +9,17 @@ function read0(inputs){
 }
 function read1(el){ 
     console.log(area1);
+   
     option = parseInt(el.id);
     console.log(option);
     sum = area1*option;
-    console.log(sum); 
+    console.log(sum);
+    //console.log(document.querySelector("#getDiv span[id]"));
+    console.log(document.querySelector("#getDiv span[id]").innerHTML);
+    document.querySelector("#getDiv span[id]").innerHTML+=sum.toString();
+    console.log(document.querySelector("#getDiv span[id]").innerHTML);
 }
 
 function read2(elem){ 
-    console.log(elem.id);
+    console.log(elem);
 }
-
-
-
